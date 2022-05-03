00965810edb90a7b74d5a745d84426a6d5478ce0
Prykon <dariomanoukian@gmail.com>
2022-05-03 09:46:10 -0300

    Improved JS

--- assets/obfuscate.js
+++ assets/obfuscate.js
@@ -1,4 +1,19 @@
 
 jQuery(document).ajaxStop(function() {
-    jQuery('#table-content tr ul a').html(`<img src="http://localhost:10033/wp-content/plugins/disciple-tools-pii-obfuscator/redacted.svg">`);
+    let redacted_css = `
+    <style>
+            .redacted {
+                background-image: url('http://localhost:10033/wp-content/plugins/disciple-tools-pii-obfuscator/redacted.svg');
+                transition: 2s width;
+                width: 0%;
+            }
+            .stroke {
+                width: 100%;
+            }
+        </style>
+    `;
+    jQuery('style').html( redacted_css );
+    let redacted_html = jQuery('#table-content').html().replaceAll('>REDACTED<', '><img src="http://localhost:10033/wp-content/plugins/disciple-tools-pii-obfuscator/redacted.svg" class="redacted"> REDACTED <');
+    jQuery('#table-content').html(redacted_html);
+    jQuery('.redacted').addClass('stroke');
 })
